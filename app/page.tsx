a3429bee13d069086fa95a862c5859f8633e20bb
mennaomar777 <mennaomar764@gmail.com>
2026-01-28 10:26:23 +0200

    implement assets searching without debounce

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,15 @@ import { data } from "./_lib/data";
 import { AssetType } from "./_interfaces/asset";
 import Sort from "./_components/Sort/Sort";
 import Filter from "./_components/Filter/Filter";
+import Search from "./_components/Search/Search";
 export default function Home() {
   const [assets, setAssets] = useState<AssetType[]>(data);
   const [sortBy, setSortBy] = useState<string>("");
   const [filterBy, setFilterBy] = useState<string>("");
+  const [search, setSearch] = useState("");
   let newAssets: AssetType[] = [...assets];
 
   // Sort
-
   if (sortBy === "price-asc")
     newAssets = newAssets.sort((a, b) => a.price - b.price);
   if (sortBy === "price-desc")
@@ -26,6 +27,12 @@ export default function Home() {
   if (filterBy)
     newAssets = newAssets.filter((asset) => asset.category === filterBy);
 
+  // Search
+  if (search) {
+    newAssets = newAssets.filter((asset) =>
+      asset.name.toLowerCase().includes(search.toLowerCase()),
+    );
+  }
   return (
     <main>
       <div className="container mx-auto max-w-[90%]">
@@ -37,6 +44,9 @@ export default function Home() {
             <p className="text-sm text-gray-600 mt-1">
               Total assets: {assets.length}
             </p>
+          </div>
+          <div>
+            <Search onSearch={setSearch} />
           </div>
           <div className="flex justify-between items-center gap-3">
             <Sort onSort={setSortBy} />
